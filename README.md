5cb4b9a93e58f78016c747c0e955015ae496b8b8
mundoenrique <mundoenrique@hotmail.com>
2022-06-22 19:26:39 -0400

    Creación de lista de categorias

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 04-git-expoert-app
+
+## links del modúlo
+
+[Estructura de archivos](https://es.reactjs.org/docs/faq-structure.html)
+
+[Estructura de los nombres de componentes](https://hackernoon.com/structuring-projects-and-naming-components-in-react-1261b6e18d76)
+
+[Estilos de la plaicación](https://gist.github.com/Klerith/e4fca6ac127ccb9abd1e8ad77fcd52f1)
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import AddCategory from './components/AddCategory';
+
 export default function App() {
+	const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+	const onAddCtegory = () => {
+		setCategories(['Valorant', ...categories]);
+	};
+
 	return (
 		<>
 			<h1>App</h1>
+			<AddCategory setCategories={setCategories} />
+
+			<ol>
+				{categories.map((category, index) => {
+					return <li key={index}>{category}</li>;
+				})}
+			</ol>
 		</>
 	);
 }
